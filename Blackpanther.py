cd500f777afda9cfe0be3341afc7488d8f42897d
KUNF24 <kunf5616@gmail.com>
2024-11-28 23:12:52 +0700

    Update Blackpanther.py

--- Blackpanther.py
+++ Blackpanther.py
@@ -161,8 +161,10 @@ for y in range(threads):
 		th.start()
 		th = threading.Thread(target = run3)
 		th.start()
-else:
 		th = threading.Thread(target = run4)
+		th.start()
+else:
+		th = threading.Thread(target = run5)
 		th.start()
 
 

